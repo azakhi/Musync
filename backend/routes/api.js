51ba0bec09a731087c13ce102931d59f5f5a040f
BerkMandiracioglu <b.mandiracioglu@ug.bilkent.edu.tr>
2019-04-13 21:33:34 +0300

    Added searchsong and getlibrary feature

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,13 +6,13 @@ const spotifyController = require('./SpotifyController');
 /* GET API listing. */
 router.post('/berk', async function(req, res, next) {
   console.log("heree");
-  let spotifyConnection = await new models.SpotifyConnection({accessToken:"",refreshToken:"xd",expiresIn:2,userId:""});
+  let spotifyConnection = await new models.SpotifyConnection({accessToken: "",refreshToken:"xd",expiresIn:2,userId:""});
   console.log("heree1");
   let playlist =await new models.SpotifyItem({id:"2D5UM1WdOtCRppu5eW7gOT"});
   console.log("heree2");
   let song = await new models.SpotifyItem({id:"2fuCquhmrzHpu5xcA1ci9x"});
   console.log("heree3"); 
-  await spotifyController.removeSong(spotifyConnection,playlist,song);
+  await spotifyController.getLibrary(spotifyConnection);
  
   console.log("heree4"); 
   res.json({});
