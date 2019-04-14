0af59f13db1d716325e96856eb559dec8736ef81
azakhi <azak.halilibrahim@gmail.com>
2019-04-14 23:24:00 +0300

    Update controllers

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,9 @@ router.post('/berk', async function(req, res, next) {
   console.log("heree");
   let spotifyConnection = await new models.SpotifyConnection({accessToken: "",refreshToken:"xd",expiresIn:2,userId:""});
   console.log("heree1");
-  let playlist =await new models.SpotifyItem({id:"2D5UM1WdOtCRppu5eW7gOT"});
+  let playlist =await new models.SpotifyItem({id:"2D5UM1WdOtCRppu5eW7gOT", uri:"", name:"", description:""});
   console.log("heree2");
-  let song = await new models.SpotifyItem({id:"2fuCquhmrzHpu5xcA1ci9x"});
+  let song = await new models.SpotifyItem({id:"2fuCquhmrzHpu5xcA1ci9x", uri:"", name:"", description:""});
   console.log("heree3"); 
   await spotifyController.getLibrary(spotifyConnection);
  
