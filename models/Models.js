8f42586a674957b43bd1aeefcffebea1e2d0f10d
azakhi <azak.halilibrahim@gmail.com>
2019-04-14 22:43:21 +0300

    Implement User model and related

--- models/Models.js
+++ models/Models.js
@@ -1,4 +1,7 @@
+const ObjectID = require('mongodb').ObjectID;
+
 const Genre = require("./Genre");
+const Location = require("./Location");
 const Place = require("./Place");
 const Playlist = require("./Playlist");
 const Song = require("./Song");
@@ -6,14 +9,19 @@ const SongRecord = require("./SongRecord");
 const SpotifyConnection = require("./SpotifyConnection");
 const SpotifyItem = require("./SpotifyItem");
 const User = require("./User");
+const VisitedPlace = require("./VisitedPlace");
 
 module.exports = {
+  ObjectID,
+  
   Genre,
+  Location,
   Place,
   Playlist,
   Song,
   SongRecord,
   SpotifyConnection,
   SpotifyItem,
-  User
+  User,
+  VisitedPlace,
 };
--- models/User.js
+++ models/User.js
@@ -2,11 +2,25 @@ const assert = require('assert');
 
 const DBBasicTypes = require("./DBBasicTypes");
 const ModelBase = require("./ModelBase");
+const SpotifyConnection = require("./SpotifyConnection");
+const Location = require("./Location");
 
 class User extends ModelBase {
   _initialize() {
     this._id = new DBBasicTypes.DBObjectID(true, null);
     this.name = new DBBasicTypes.DBString(true, "");
+    this.lastLogin = new DBBasicTypes.DBDate(true, Date.now());
+    this.points = new DBBasicTypes.DBNumber(true, 0);
+    this.location = new Location();
+    this.visitedPlaces = new DBBasicTypes.DBArray(true, []);
+    this.requestedSongs = new DBBasicTypes.DBArray(true, []);
+    this.isRegistered = new DBBasicTypes.DBBoolean(true, false);
+    this.spotifyConnection = new SpotifyConnection();
+    this.email = new DBBasicTypes.DBString(true, "");
+    this.password = new DBBasicTypes.DBString(true, "");
+    this.premiumEnd = new DBBasicTypes.DBDate(true, null);
+    this.premiumTier = new DBBasicTypes.DBNumber(true, 0);
+    this.places = new DBBasicTypes.DBArray(true, []);
   }
   
   static get collection() {
