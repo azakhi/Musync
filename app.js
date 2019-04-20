72680643ab91e1d483480ff60650540a33af2165
ahmetcandiroglu <ahmetcandiroglu@gmail.com>
2019-04-20 19:08:44 +0300

    Fix CORS error and change default port

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const MainController = require('./routes/MainController');
 const placeController = require('./routes/PlaceController');
 const userController = require('./routes/UserController');
@@ -17,10 +15,6 @@ DBManager.connect(function(){});
 const ModelManager = require("./models/ModelManager");
 
 const app = express();
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,8 +28,11 @@ app.use(session({
   cookie: { secure: false /*till https*/ },
   maxAge: Date.now() + (60 * 60 * 1000),
 }));
-
 app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
   res.on("finish", function() {
     // Commit DB changes
     ModelManager.commitChanges();
@@ -44,8 +41,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+
 app.use('/place', placeController);
 app.use('/user', userController);
 app.use('/', MainController);
