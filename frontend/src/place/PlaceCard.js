c2c0a1660f664d46eaf32de371497bb4c1d58e1f
anilerken <anilerken@users.noreply.github.com>
2019-04-23 16:30:20 +0300

    Implemented Enter PIN Code UI

--- frontend/src/place/PlaceCard.js
+++ frontend/src/place/PlaceCard.js
@@ -8,21 +8,39 @@ import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 
 class PlaceCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     const {place, type} = this.props;
     const {id, name, image, genres, currentSong} = place;
-    
+
     const isTypePrimary = (type === PlaceCardTypes.HomeViewPrimary);
     const isTypeSecondary = (type === PlaceCardTypes.HomeViewSecondary);
     const isTypePlaceView = (type === PlaceCardTypes.PlaceView);
-  
+
     const showMedia = (isTypePrimary || isTypePlaceView);
     const imageHeight = isTypePrimary ? "auto" : 150;
     const showCardActions = (isTypePrimary || isTypeSecondary);
-  
+
     return (
       <Grid item xs={12} key={id}>
         <Card square elevation={isTypePrimary ? 2 : 1}>
@@ -35,7 +53,7 @@ class PlaceCard extends Component {
                        title={name + " image"}
                        style={{objectFit:"cover"}}/>
           }
-        
+
           <CardContent>
             <Typography variant="h5" align="center" gutterBottom>
               {name}
@@ -48,19 +66,47 @@ class PlaceCard extends Component {
               {" Now Playing: " + currentSong}
             </Typography>
           </CardContent>
-        
+
           {
             showCardActions &&
             <CardActions style={{justifyContent: 'center'}}>
               <Button color="primary"
                       size="small"
                       variant={isTypePrimary ? "contained" : "text"}
-                      onClick={()=>{console.log('I am here: ' + name)}}>
+                      onClick={this.handleClickOpen}>
                 I am here!
               </Button>;
             </CardActions>
           }
         </Card>
+
+        <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogContent>
+              <DialogContentText>
+                Enter the PIN Code of the Place
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label=""
+                type="text"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={this.handleClose} color="primary">
+                Join Place
+              </Button>
+            </DialogActions>
+          </Dialog>
       </Grid>
     );
   }
