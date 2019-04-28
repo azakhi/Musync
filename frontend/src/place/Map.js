198067fe0a47786e2867587c3e9316405c637465
BerkMandiracioglu <b.mandiracioglu@ug.bilkent.edu.tr>
2019-04-28 22:48:44 +0300

    fixed a security issue

--- frontend/src/place/Map.js
+++ frontend/src/place/Map.js
@@ -21,7 +21,7 @@ class Map extends Component {
     if (!window.google) {
       var s = document.createElement('script');
       s.type = 'text/javascript';
-      s.src = `https://maps.google.com/maps/api/js?key=AIzaSyCGRiGhg4FU8Hg0qNpaj7F58MBx1eltYF0&libraries=places`;
+      s.src = `https://maps.google.com/maps/api/js?key=API_KEY&libraries=places`;
       var x = document.getElementsByTagName('script')[0];
       x.parentNode.insertBefore(s, x);
       // Below is important. 
